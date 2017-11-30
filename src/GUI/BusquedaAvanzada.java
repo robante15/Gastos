c8b1b4cba173064423cc2d2f8549c10f1ae3a639
Julio Eduardo Pastore Baires <julio.pastore.jp@gmail.com>
2017-11-29 22:17:30 -0600

    Boton atras busqueda avanzada

--- src/GUI/BusquedaAvanzada.java
+++ src/GUI/BusquedaAvanzada.java
@@ -98,6 +98,7 @@ public class BusquedaAvanzada extends javax.swing.JFrame {
         lbl_desde = new javax.swing.JLabel();
         lbl_hasta = new javax.swing.JLabel();
         btn_todo = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
@@ -167,6 +168,14 @@ public class BusquedaAvanzada extends javax.swing.JFrame {
         });
         getContentPane().add(btn_todo, new org.netbeans.lib.awtextra.AbsoluteConstraints(50, 160, -1, -1));
 
+        jButton1.setText("Atras");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 30, -1, -1));
+
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/5.jpg"))); // NOI18N
         getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(-620, -200, -1, -1));
 
@@ -232,6 +241,11 @@ public class BusquedaAvanzada extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_btn_buscarActionPerformed
 
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        this.dispose();
+    }//GEN-LAST:event_jButton1ActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -273,6 +287,7 @@ public class BusquedaAvanzada extends javax.swing.JFrame {
     private javax.swing.ButtonGroup btnGroup_selector;
     private javax.swing.JButton btn_buscar;
     private javax.swing.JButton btn_todo;
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lbl_desde;
